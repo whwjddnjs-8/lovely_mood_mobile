1f004a0eb24691c4b459a695482ef5c4a9499adf
조정원 <whwjddnjs8@gmail.com>
2022-12-13 23:49:27 +0900

    cart

--- lovely mood/js/custom.js
+++ lovely mood/js/custom.js
@@ -1,11 +1,10 @@
 $(function () {
 	/* Load Include Html */
-	$('head').load('/include/head.html')
 	$('.gnb-area').load('/include/gnb.html')
 	$('.main-header-area').load('/include/main-header.html')
 
 	/* Search Recent View Product Slider */
-	$('.recent-view-items').slick({
+	$('.recent-view .recent-view-items').slick({
 		slidesToShow: 4,
 		slidesToScroll: 1,
 		dots: false,
@@ -16,5 +15,14 @@ $(function () {
 	/* Product Like 좋아요 버튼 toggle */
 	$('.product-like').click(function () {
 		$(this).toggleClass('active')
+	})
+
+	/* Cart delete 버튼 */
+	$('.add-cart .cart-delete-all').click(function () {
+		$('.cart-items').hide()
+	})
+
+	$('.cart-items .item .btn-close').click(function () {
+		$(this).parent().parent().hide()
 	})
 })
